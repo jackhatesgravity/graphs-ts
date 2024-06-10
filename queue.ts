da31ca3b984a1c94d82ce526a6e8aaa2f1ade429
jackhatesgravity <18660090+jackhatesgravity@users.noreply.github.com>
2024-06-10 09:48:40 +0800

    Added Queue functionality

--- queue.ts
+++ queue.ts
@@ -1,34 +1,34 @@
-interface StackInterface<T> {
+interface QueueInterface<T> {
     count(): number;
     empty(): boolean;
     put(node: T): number;
     get(): T | undefined;
 }
 
-class Stack<T> implements StackInterface<T> {
-    private _stack: T[];
+class Queue<T> implements QueueInterface<T> {
+    private _queue: T[];
 
     constructor() {
-        this._stack = [];
+        this._queue = [];
     }
 
     // Get number of elements in the Stack.
     public count(): number {
-        return this._stack.length;
+        return this._queue.length;
     }
 
     // Check if the Stack is empty.
     public empty(): boolean {
-        return this._stack.length === 0;
+        return this._queue.length === 0;
     }
 
     // Add an element to the top of the Stack
     public put(node: T): number {
-        return this._stack.unshift(node);
+        return this._queue.push(node);
     }
 
     // Removes and returns the top element of the Stack.
     public get(): T | undefined {
-        return this._stack.shift();
+        return this._queue.pop();
     }
 }
