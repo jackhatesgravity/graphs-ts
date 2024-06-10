e7f47d1b06d07ba65cadf5d350ea5abfbb8a3c9a
jackhatesgravity <18660090+jackhatesgravity@users.noreply.github.com>
2024-06-10 12:39:15 +0800

    Playing around with type enforcement on attrs

--- graph.ts
+++ graph.ts
@@ -18,28 +18,36 @@ class Edge {
         return this._attrs;
     }
 
+    // Main attribute I care about is 'weight' but I'm following the NetworkX code pretty closely to allow expansion.
     update_attrs(attrs: Record<string, any>) {
         Object.assign(this._attrs, attrs);
     }
 
-    // Should we return anything here?
+    // Should we return anything here? And should we add single attr removal?
     clear_attrs(): void {
         this._attrs = null;
     }
 }
 
+// Learning experience, but I think this is a reasonably good start.
+// Basically just trying to put a limit of some kind on the attribute values.
+// Come back and do this better, it's deliberate loose for now.
+interface Attribute {
+    [key: string]: any;
+}
+
 interface GraphInterface {
-    add_node(node: Node, attrs?: Record<string, any>): void;
+    add_node(node: Node, attrs?: Record<string, string>): void;
     remove_node(node: Node): void;
     add_edge(u: Node, v: Node, attrs?: Record<string, any>): void;
     remove_edge(u: Node, v: Node): void;
     has_node(node: Node): boolean;
-    print_nodes(): void;
+    print_node_ids(): void;
 }
 
 class Graph implements GraphInterface {
 
-    private _node: Map<Node, Record<string, any>>;
+    private _node: Map<Node, Attribute>;
     private _adj: Map<Node, Map<Node, Edge>>;
 
     constructor() {
@@ -47,7 +55,7 @@ class Graph implements GraphInterface {
         this._adj = new Map();
     }
 
-    add_node(node: Node, attrs: Record<string, any> = {}): void {
+    add_node(node: Node, attrs: Attribute = {}): void {
         if (!node) {
             throw new Error("Node cannot be null or undefined!");
         }
@@ -74,20 +82,21 @@ class Graph implements GraphInterface {
         throw new Error('Method not implemented.');
     }
 
-    print_nodes(): void {
-        this._node.forEach((_, node: Node) => {
-            console.log(node.get_id());
+    print_node_ids(): void {
+        this._node.forEach((element: Attribute, node: Node) => {
+            console.log(`id: ${node.get_id()}, attrs: ${JSON.stringify(element)}`);
         });
     }
 }
 
 const graph = new Graph();
+
 const node1 = new Node("node_1");
 const node2 = new Node("node_2");
 const node3 = new Node("node_3");
 
-graph.add_node(node1);
-graph.add_node(node2);
-graph.add_node(node3);
+graph.add_node(node1, {"colour":"red", "size":10});
+graph.add_node(node2, {"colour":"blue", "shape":"circle"});
+graph.add_node(node3, {"colour":"yellow","line":"dotted"});
 
-graph.print_nodes();
+graph.print_node_ids();
