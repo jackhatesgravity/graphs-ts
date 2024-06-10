e43e58a2ade2a790a011017c30c45e07da5f3015
jackhatesgravity <18660090+jackhatesgravity@users.noreply.github.com>
2024-06-10 09:40:24 +0800

    Added basic Stack functionality

--- stack.ts
+++ stack.ts
@@ -1,13 +1,34 @@
-class Stack {
-    private _stack: any[];
+interface StackInterface<T> {
+    count(): number;
+    empty(): boolean;
+    put(node: T): number;
+    get(): T | undefined;
+}
+
+class Stack<T> implements StackInterface<T> {
+    private _stack: T[];
+
     constructor() {
         this._stack = [];
     }
-    empty = (): boolean => {
+
+    // Get number of elements in the Stack.
+    public count(): number {
+        return this._stack.length;
+    }
+
+    // Check is the Stack is empty.
+    public empty(): boolean {
         return this._stack.length === 0;
     }
+
+    // Add element to the top of the Stack
+    public put(node: T): number {
+        return this._stack.unshift(node);
+    }
+
+    // Removes and returns the top element of the Stack.
+    public get(): T | undefined {
+        return this._stack.pop();
+    }
 }
-
-
-const myStack = new Stack();
-console.log(myStack.empty());
