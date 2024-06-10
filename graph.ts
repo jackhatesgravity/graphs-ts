ac56058283481148dd30d0cf7d2477557da2160c
jackhatesgravity <18660090+jackhatesgravity@users.noreply.github.com>
2024-06-10 13:12:10 +0800

    Improved debug and logging, added log-timestamp dependency

--- graph.ts
+++ graph.ts
@@ -1,10 +1,18 @@
-import { Stack } from './stack';
-import { Queue } from './queue';
+import {Stack} from './stack';
+import {Queue} from './queue';
+
+require('log-timestamp');
 
 class Node {
     private readonly _id: string;
-    constructor(id: string) {this._id = id}
-    get_id() {return this._id;}
+
+    constructor(id: string) {
+        this._id = id
+    }
+
+    get_id() {
+        return this._id;
+    }
 }
 
 class Edge {
@@ -38,10 +46,15 @@ interface Attribute {
 
 interface GraphInterface {
     add_node(node: Node, attrs?: Record<string, string>): void;
+
     remove_node(node: Node): void;
+
     add_edge(u: Node, v: Node, attrs?: Record<string, any>): void;
+
     remove_edge(u: Node, v: Node): void;
+
     has_node(node: Node): boolean;
+
     print_node_ids(): void;
 }
 
@@ -55,7 +68,8 @@ class Graph implements GraphInterface {
         this._adj = new Map();
     }
 
-    add_node(node: Node, attrs: Attribute = {}): void {
+    // Returns the size of the node array.
+    add_node(node: Node, attrs: Attribute = {}): number {
         if (!node) {
             throw new Error("Node cannot be null or undefined!");
         }
@@ -64,28 +78,50 @@ class Graph implements GraphInterface {
             this._node.set(node, attrs);
             this._adj.set(node, new Map());
         } else {
+            console.warn(`Node ${node.get_id()} already in graph. Updating attributes.`)
             const existingAttrs = this._node.get(node);
             Object.assign(existingAttrs, attrs);
         }
+        console.debug(`Successfully added node ${node.get_id()}.`);
+        return this._node.size;
     }
 
-    remove_node(): Node {
-        throw new Error('Method not implemented.');
+    // Returns the size of the node array.
+    // This will need to be updated when we add edges to also completely remove the node from the adj list.
+    remove_node(node: Node): number {
+        if (!node) {
+            throw new Error("Node cannot be null or undefined!");
+        }
+
+        if (!this._node.has(node)) {
+            throw new Error("Node not in graph!");
+        } else {
+            console.warn(`Removing node ${node.get_id()} from graph.`);
+            this._node.delete(node);
+            this._adj.delete(node);
+        }
+        console.debug(`Successfully removed node ${node.get_id()}.`);
+        return this._node.size;
     }
+
     add_edge(node: Node, other: Node): Edge {
         throw new Error('Method not implemented.');
     }
+
     remove_edge(node: Node, other: Node): Edge {
         throw new Error('Method not implemented.');
     }
+
     has_node(node: Node): boolean {
         throw new Error('Method not implemented.');
     }
 
     print_node_ids(): void {
+        const entries: string[] = [];
         this._node.forEach((element: Attribute, node: Node) => {
-            console.log(`id: ${node.get_id()}, attrs: ${JSON.stringify(element)}`);
+            entries.push(`id: ${node.get_id()}, attrs: ${JSON.stringify(element)}`);
         });
+        console.debug("Printing all nodes in graph: " + entries.join(', ') + ".");
     }
 }
 
@@ -95,8 +131,14 @@ const node1 = new Node("node_1");
 const node2 = new Node("node_2");
 const node3 = new Node("node_3");
 
-graph.add_node(node1, {"colour":"red", "size":10});
-graph.add_node(node2, {"colour":"blue", "shape":"circle"});
-graph.add_node(node3, {"colour":"yellow","line":"dotted"});
+graph.add_node(node1, {"colour": "red", "size": 10});
+graph.add_node(node2, {"colour": "blue", "shape": "circle"});
+graph.add_node(node3, {"colour": "yellow", "line": "dotted"});
 
-graph.print_node_ids();
+graph.remove_node(node1)
+
+graph.add_node(node1, {"colour": "red"})
+graph.add_node(node1, {"size": 10})
+graph.add_node(node1, {"colour": "green"})
+
+
